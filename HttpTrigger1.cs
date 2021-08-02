224231ca0314812a11b2f8f43219ff21592ada76
Yugi-Seong <77322860+Yugi-Seong@users.noreply.github.com>
2021-08-02 17:15:18 +0900

    2st

--- HttpTrigger1.cs
+++ HttpTrigger1.cs
@@ -1,4 +1,3 @@
-  
 using System;
 using System.IO;
 using System.Threading.Tasks;
@@ -8,7 +7,10 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Azure.Storage.Blobs;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Function
 {
@@ -21,34 +23,45 @@ namespace Function
         {
             string connStrA = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
             string requestBody = new StreamReader(req.Body).ReadToEnd();
-            string datetime = DateTime.UtcNow.AddHours(9).ToString("yyyyMMdd");
+            string datetime = DateTime.UtcNow.AddHours(9).ToString("yyyyMMdd hh:mm");
             
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            JObject objB = (JObject)JsonConvert.DeserializeObject(requestBody);
 
             BlobServiceClient clientA = new BlobServiceClient(connStrA);
             BlobContainerClient containerA = clientA.GetBlobContainerClient("yugicon");
             BlobClient blobA = containerA.GetBlobClient(datetime + ".json");
 
-            //string responseA = "No Data";
+            string responseA = "No Data";
+            JObject ObjA = new JObject();
 
-//            if (blobA.Exists())
-//            {
-//                using (MemoryStream msA = new MemoryStream())
-//                {
-//                    blobA.DownloadTo(msA);
-//                    responseA = System.Text.Encoding.UTF8.GetString(msA.ToArray());
-//                }
- //           }
+            if (blobA.Exists())
+            {
+                using (MemoryStream msA = new MemoryStream())
+                {
+                    blobA.DownloadTo(msA);
+                    responseA = System.Text.Encoding.UTF8.GetString(msA.ToArray());
+                    ObjA = JObject.Parse(responseA);
+                }
+            }
+
+            
+                IList<string> keys = objB.Properties().Select(p => p.Name).ToList();    
+                foreach (var item in keys)
+                {
+                    ObjA.Add(item, objB[item]);
+                }
+            
+            
 
             // string tagData = Newtonsoft.Json.JsonConvert.SerializeObject(responseA);
-           // string tagData = Newtonsoft.Json.JsonConvert.SerializeObject(data);
+            string tagData = Newtonsoft.Json.JsonConvert.SerializeObject(ObjA);
             
-            using (Stream streamA = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(data)))
+            using (Stream streamA = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(tagData)))
             {
                 blobA.Upload(streamA, true);
             }
             
-            return data;
+            return requestBody;
         }
     }
 }
